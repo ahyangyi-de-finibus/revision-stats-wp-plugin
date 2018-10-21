09aac548fe07d4a5c42e37ae51656d89db15c1a3
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2018-10-22 02:33:35 +0800

    Initial commit (source SHA-1 7fd4f978)

---
+++ revision-stats.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* Plugin Name: Revision Stats
+* Description: Include revision stats with shortcode [revision_stats]
+* Version: 1.0
+* Author: Yi Yang
+* Author URI: https://blog.ahyangyi.org
+*/
+
+function add_revision_stats(){
+$dir = plugin_dir_url( __FILE__ );
+
+wp_enqueue_style("revision_stats", $dir . "/" . "styles.e12c2598acce6741fd25.css");
+?>
+<revision-stats></revision-stats>
+<script type="text/javascript" src="<?php echo $dir . "/" . "runtime.4ccf67c919f9077afc3e.js"; ?>"></script>
+<script type="text/javascript" src="<?php echo $dir . "/" . "polyfills.f6ae3e8b63939c618130.js"; ?>"></script>
+<script type="text/javascript" src="<?php echo $dir . "/" . "main.23bca382f4b36cc02732.js"; ?>"></script>
+<?php
+}
+add_shortcode('revision_stats', 'add_revision_stats');
+?>
