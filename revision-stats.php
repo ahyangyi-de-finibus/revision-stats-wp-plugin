6cb6b9b1c8484ab34adf2600539ba7f24e1febe4
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2018-10-24 01:27:51 +0800

    Bump version (Commit SHA-1 c02058c)

--- revision-stats.php
+++ revision-stats.php
@@ -10,11 +10,11 @@
 function add_revision_stats(){
 $dir = plugin_dir_url( __FILE__ );
 
-wp_enqueue_style("revision_stats", $dir . "/" . "styles.f18bf8bf76e6f4f89f0a.css");
+wp_enqueue_style("revision_stats", $dir . "/" . "styles.4475941bb81c68bc6671.css");
 return "<revision-stats></revision-stats>" .
     "<script type=\"text/javascript\" src=\"" . $dir . "/runtime.4ccf67c919f9077afc3e.js\"></script>" .
     "<script type=\"text/javascript\" src=\"" . $dir . "/polyfills.f6ae3e8b63939c618130.js\"></script>" .
-    "<script type=\"text/javascript\" src=\"" . $dir . "/main.3252d3a684acbd3296ff.js\"></script>";
+    "<script type=\"text/javascript\" src=\"" . $dir . "/main.41aeaac3b19dd2dff786.js\"></script>";
 }
 add_shortcode('revision_stats', 'add_revision_stats');
 ?>
