0f3b1202a548bc199b6fab142e67c8cf954591ad
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2018-10-28 15:46:13 +0800

    Bump to Angular 7 ( Commit SHA-1 83de9b97 )

--- revision-stats.php
+++ revision-stats.php
@@ -10,11 +10,11 @@
 function add_revision_stats(){
 $dir = plugin_dir_url( __FILE__ );
 
-wp_enqueue_style("revision_stats", $dir . "/" . "styles.4475941bb81c68bc6671.css");
+wp_enqueue_style("revision_stats", $dir . "/" . "styles.d33ff9b795c1f5d07a02.css");
 return "<revision-stats></revision-stats>" .
     "<script type=\"text/javascript\" src=\"" . $dir . "/runtime.4ccf67c919f9077afc3e.js\"></script>" .
-    "<script type=\"text/javascript\" src=\"" . $dir . "/polyfills.f6ae3e8b63939c618130.js\"></script>" .
-    "<script type=\"text/javascript\" src=\"" . $dir . "/main.41aeaac3b19dd2dff786.js\"></script>";
+    "<script type=\"text/javascript\" src=\"" . $dir . "/polyfills.d1c7bf4a2ae7c3435f95.js\"></script>" .
+    "<script type=\"text/javascript\" src=\"" . $dir . "/main.45005b10be1254935cb6.js\"></script>";
 }
 add_shortcode('revision_stats', 'add_revision_stats');
 ?>
