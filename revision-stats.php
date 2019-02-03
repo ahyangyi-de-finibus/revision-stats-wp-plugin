27d4d290078f38fdda15169fed63503097002bab
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2019-02-03 16:35:16 +0800

    Um... the last commit was wrong

--- revision-stats.php
+++ revision-stats.php
@@ -12,9 +12,9 @@ function add_revision_stats() {
 
     wp_enqueue_style("revision_stats", $dir . "/" . "styles.d5290cc6a5c35922a51d.css");
     return "<revision-stats></revision-stats>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/main.87adf438603d63ea79c9.js\"></script>" .
+        "<script type=\"text/javascript\" src=\"" . $dir . "/runtime.95ba26ba764e20834375.js\"></script>" .
         "<script type=\"text/javascript\" src=\"" . $dir . "/polyfills.cea9bfca28447a75b26b.js\"></script>" .
-        "<script type=\"text/javascript\" src=\"" . $dir . "/main.c2a8bbff7c161be8b9c0.js\"></script>";
+        "<script type=\"text/javascript\" src=\"" . $dir . "/main.87adf438603d63ea79c9.js\"></script>";
 }
 
 add_shortcode('revision_stats', 'add_revision_stats');
