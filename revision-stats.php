05505909c368a49a65951f2407dc1ca5af0128ce
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2019-08-31 15:04:16 +0800

    Fix path

--- revision-stats.php
+++ revision-stats.php
@@ -10,7 +10,7 @@
 function add_revision_stats() {
     $dir = plugin_dir_url( __FILE__ );
 
-    wp_enqueue_style("revision_stats", $dir . "/" . "styles.d5290cc6a5c35922a51d.css");
+    wp_enqueue_style("revision_stats", $dir . "/" . "styles.8f52370cd7dd7dba1e8d.css");
     return "<revision-stats></revision-stats>" .
         "<script type=\"text/javascript\" src=\"" . $dir . "/runtime-es5.c8ae985b640079ae0993.js\"></script>" .
         "<script type=\"text/javascript\" src=\"" . $dir . "/polyfills-es5.6edbdb9a7de91e0784c7.js\"></script>" .
